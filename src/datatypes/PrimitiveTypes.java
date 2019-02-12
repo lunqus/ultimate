957e7d702faddee94e1dbae6ff0e16ae9251cc58
LUNKUS <sigitas@lunkus.com>
2019-02-12 07:20:04 +0200

    primitive data types

---
+++ src/datatypes/PrimitiveTypes.java
@@ -0,0 +1,30 @@
+package datatypes;
+
+public class PrimitiveTypes {
+
+    public static void main(String[] args) {
+
+        byte myByte = 2;            // 8 Bit
+        short myShort = 5;          // 16 Bit
+        int myInt = 20;             // 32 bit
+        long myLong = 123;          // 64 Bit
+        float myFloat = 3.6f;       // 32 Bit
+        double myDouble = 456.44;   // 64 bit
+        boolean myBool = true;      // 16 Bit - ?
+        char myChar = 'A';          // 8 Bit - ?
+
+
+        int sum = (int) (myInt + myDouble); // 40.25 --> 40
+
+        boolean isRainy = true; // 0 = false, 1 = true
+        if (myBool) {
+            System.out.println("You need an umbrella");
+        }
+
+        in nine = 9;
+        char notNine = '9'; //
+
+
+
+    }
+}
