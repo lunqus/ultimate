df6c827504e8fb3772a8d9e7ba27e65829f08e5e
LUNKUS <sigitas@lunkus.com>
2019-02-16 18:51:53 +0200

    work arround with functions

---
+++ src/javasyntax/Functions.java
@@ -0,0 +1,33 @@
+package javasyntax;
+
+public class Functions {
+
+    public static void main(String[] args) {
+
+        int a = 2;
+        int b = 3;
+        int add = a + b;
+        System.out.println(add);
+
+        boolean isPrime = isNumberPrime(7);
+        System.out.println(isPrime);
+    }
+
+    static int add(int a, int b) {
+        int sum = a + b;
+        return sum;
+    }
+
+    static boolean isNumberPrime(int a) {
+
+        for (int i = 2; i < a; i++) {
+
+            if (a % i == 0) {
+
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
