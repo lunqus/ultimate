e57e6265199616052a0f0355dfea6863c5d8502b
LUNKUS <sigitas@lunkus.com>
2019-02-16 08:06:15 +0200

    primitive data types finish

--- src/datatypes/PrimitiveTypes.java
+++ src/datatypes/PrimitiveTypes.java
@@ -4,8 +4,8 @@ public class PrimitiveTypes {
 
     public static void main(String[] args) {
 
-        byte myByte = 2;            // 8 Bit
-        short myShort = 5;          // 16 Bit
+        byte myByte = 2;            // 8 Bit (-127 - +127)
+        short myShort = 5;          // 16 Bit (2 x byte)
         int myInt = 20;             // 32 bit
         long myLong = 123;          // 64 Bit
         float myFloat = 3.6f;       // 32 Bit
@@ -21,10 +21,37 @@ public class PrimitiveTypes {
             System.out.println("You need an umbrella");
         }
 
-        in nine = 9;
-        char notNine = '9'; //
+        int nine = 9;
+        char otherChar = 'A';
+        // A ---> 000001111 -------> number 57 ------> A
+        // 9 -----> number 37 ------> 000001 (example)
+        // number 9 -----> 00001001
 
+        int mappedVal = (int) otherChar;
+        System.out.println(mappedVal); // prints -----> 57
 
+        char s = 's';
+        char i = 'i';
+        char g = 'g';
+
+        int maps = (int) s;
+        int mapi = (int) i;
+        int mapg = (int) g;
+
+        System.out.println(maps + " " + mapi + " " + mapg + " " + mapi + " " + maps);
+
+        int d = 865;
+        int si = 115;
+
+        char prefix = (char) d;
+
+        System.out.println(prefix);
+
+        int f = 20;
+        double r = 22;
+
+        double rez = f / r;
+        System.out.println(rez);
 
     }
 }
