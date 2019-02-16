a7a33b9f5e53eb1cad8ff3aa1c80ab7a0f236af1
LUNKUS <sigitas@lunkus.com>
2019-02-16 09:36:34 +0200

    if else ... statements

---
+++ src/datatypes/ifStatements.java
@@ -0,0 +1,29 @@
+package datatypes;
+
+public class ifStatements {
+
+    public static void main(String[] args) {
+
+        if (4 == 3) {
+            System.out.println("We are inside our if statement");
+        } else {
+
+            System.out.println("We are inside else statement");
+        }
+
+
+        double defaultTicketPrice = 10.0;
+        double discout = .1;
+        double ticketPrice = defaultTicketPrice;
+        int age = 26;
+
+        if (age < 18) {
+            ticketPrice = ticketPrice - ticketPrice * discout;
+        } else if ( age < 25 ) {
+                ticketPrice = ticketPrice - ticketPrice * discout/2;
+            }
+
+        System.out.println(ticketPrice);
+
+    }
+}
